a7d6928dffe93b681deb5fe5faf49fc6bbccbb40
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-11-27 16:50:11 +0200

    move storage handling to a different package use S3CompatBucketURLOpener to open the bucket that was supplied to the NewS3CompatLogStore function

--- store/s3.go
+++ store/s3.go
@@ -2,11 +2,8 @@ package store
 
 import (
 	"context"
-	"fmt"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	awsv2cfg "github.com/aws/aws-sdk-go-v2/config"
-	s3v2 "github.com/aws/aws-sdk-go-v2/service/s3"
-	goblob "gocloud.dev/blob"
+	"github.com/csimplestring/delta-go/storage"
+	"gocloud.dev/blob"
 	"net/url"
 	"strings"
 	"sync"
@@ -15,8 +12,6 @@ import (
 	"github.com/csimplestring/delta-go/internal/util/path"
 	"github.com/csimplestring/delta-go/iter"
 	"github.com/rotisserie/eris"
-
-	"gocloud.dev/blob/s3blob"
 )
 
 // Note: currently s3 log store only supports single driver.
@@ -29,7 +24,7 @@ func NewS3LogStore(logDir string) (*S3SingleDriverLogStore, error) {
 		return nil, err
 	}
 
-	bucket, err := goblob.OpenBucket(context.Background(), blobURL)
+	bucket, err := blob.OpenBucket(context.Background(), blobURL)
 	if err != nil {
 		return nil, err
 	}
@@ -52,46 +47,16 @@ func NewS3LogStore(logDir string) (*S3SingleDriverLogStore, error) {
 	}, nil
 }
 
-type compatBucketURLOpener struct {
-	cfg      aws.Config
-	awsProps AWSProperties
-}
-
-func (cbuo compatBucketURLOpener) OpenBucketURL(ctx context.Context, u *url.URL) (*goblob.Bucket, error) {
-	clientV2 := s3v2.NewFromConfig(cbuo.cfg, func(o *s3v2.Options) {
-		o.BaseEndpoint = aws.String(cbuo.awsProps.Endpoint)
-		o.EndpointOptions.DisableHTTPS = cbuo.awsProps.DisableHTTPs
-		o.UsePathStyle = cbuo.awsProps.ForcePathStyle
-	})
-	return s3blob.OpenBucketV2(ctx, clientV2, u.Host, nil)
-}
-
-func RegisterS3CompatBucketURLOpener(scheme string, awsProps *AWSProperties) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(r)
-		}
-	}()
+func NewS3CompatLogStore(awsProps *storage.AWSProperties, u *url.URL) (*S3SingleDriverLogStore, error) {
 	ctx := context.Background()
-	cfg, _ := GenerateConfig(ctx, awsProps)
-	cbuo := compatBucketURLOpener{
-		cfg:      cfg,
-		awsProps: *awsProps,
+	cfg, err := storage.GenerateConfig(ctx, awsProps)
+	bucketOpener := storage.NewS3CompatBucketURLOpener(cfg, *awsProps)
+	bucket, err := bucketOpener.OpenBucketURL(ctx, u)
+	if err != nil {
+		return nil, err
 	}
-	goblob.DefaultURLMux().RegisterBucket(scheme, &cbuo)
-}
-
-func NewS3CompatLogStore(awsProps *AWSProperties, bucketName, path string) (*S3SingleDriverLogStore, error) {
-	ctx := context.Background()
-	cfg, err := GenerateConfig(ctx, awsProps)
-	clientV2 := s3v2.NewFromConfig(cfg, func(o *s3v2.Options) {
-		o.BaseEndpoint = aws.String(awsProps.Endpoint)
-		o.EndpointOptions.DisableHTTPS = awsProps.DisableHTTPs
-		o.UsePathStyle = awsProps.ForcePathStyle
-	})
-	bucket, err := s3blob.OpenBucketV2(ctx, clientV2, bucketName, nil)
-	logDir := handleLogDirPath(path)
-	bucket = goblob.PrefixedBucket(bucket, logDir)
+	logDir := handleLogDirPath(u.Path)
+	bucket = blob.PrefixedBucket(bucket, logDir)
 	if err != nil {
 		return nil, err
 	}
@@ -113,33 +78,16 @@ func NewS3CompatLogStore(awsProps *AWSProperties, bucketName, path string) (*S3S
 }
 
 func handleLogDirPath(path string) string {
+	path = strings.TrimPrefix(path, "/")
 	if strings.HasSuffix(path, "_delta_log/") {
 		return path
 	} else if strings.HasSuffix(path, "_delta_log") {
 		return path + "/"
+	} else if strings.HasSuffix(path, "/") {
+		return path + "_delta_log/"
 	} else {
 		return path + "/_delta_log/"
 	}
-}
-
-type AWSProperties struct {
-	Region         string
-	ForcePathStyle bool
-	DisableHTTPs   bool
-	CredsProvider  aws.CredentialsProvider
-	Endpoint       string
-}
-
-func GenerateConfig(ctx context.Context, awsProps *AWSProperties) (aws.Config, error) {
-	conf, err := awsv2cfg.LoadDefaultConfig(ctx,
-		awsv2cfg.WithDefaultRegion("us-east-1"),
-		awsv2cfg.WithRegion(awsProps.Region),
-		awsv2cfg.WithCredentialsProvider(awsProps.CredsProvider),
-	)
-	if err != nil {
-		return aws.Config{}, err
-	}
-	return conf, nil
 }
 
 type S3SingleDriverLogStore struct {
